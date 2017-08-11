adbec5ce11e80ac02260e78365f076e993518daa
yoggy <yoggy0@gmail.com>
2017-08-11 18:11:13 +0900

    ...

--- README.md
+++ README.md
@@ -8,15 +8,20 @@ Usage
     $ gem install mqtt
     $ git clone https://github.com/yoggy/mqtt_router.git
     $ cd mqtt_router
-    $ cp mqtt_router_config.yaml.sample mqtt_router_config.yaml
-    $ vi mqtt_router_config.yaml
+    $ cp config.yaml.sample config.yaml
+    $ vi config.yaml
     
-        mqtt_host:     mqtt.example.com
+        mqtt_host:     iot.eclipse.org
         mqtt_port:     1883
+        mqtt_use_auth: false
         mqtt_username: username
         mqtt_password: password
+        mqtt_subscribe_topic: subscribe_topic
+        mqtt_subscribe_regexp: co2\":(.+),
+        mqtt_publish_topic: publish_topic
+        mqtt_publish_format: segd#{sprintf("%04d",md[1].to_i)}
     
-    $ ./mqtt_router office_co2 7seg0003 'co2\":(.+),' 'segd#{sprintf("%04d",md[1].to_i)}'
+    $ ./mqtt_router config.yaml'
     
 Dataflow
 ----
--- mqtt_router.rb
+++ mqtt_router.rb
@@ -6,15 +6,20 @@
 #   $ gem install mqtt
 #   $ git clone https://github.com/yoggy/mqtt_router.git
 #   $ cd mqtt_router
-#   $ cp mqtt_router_config.yaml.sample mqtt_router_config.yaml
-#   $ vi mqtt_router_config.yaml
+#   $ cp config.yaml.sample config.yaml
+#   $ vi config.yaml
 #   
 #       mqtt_host:     mqtt.example.com
 #       mqtt_port:     1883
+#       mqtt_use_auth: false
 #       mqtt_username: username
 #       mqtt_password: password
+#       mqtt_subscribe_topic: subscribe_topic
+#       mqtt_subscribe_regex: co2\":(.+),
+#       mqtt_publish_topic: publish_topic
+#       mqtt_publish_format: segd#{sprintf("%04d",md[1].to_i)}
 #
-#   $ ./mqtt_router office_co2 7seg0003 'co2\":(.+),' 'segd#{sprintf("%04d",md[1].to_i)}'
+#   $ ./mqtt_router ./config.yaml
 #
 # License:
 #   Copyright (c) 2016 yoggy <yoggy0@gmail.com>
@@ -33,38 +38,52 @@ Dir.chdir(File.dirname($0))
 $log = Logger.new(STDOUT)
 $log.level = Logger::DEBUG
 
-$conf = OpenStruct.new(YAML.load_file(File.dirname($0) + '/mqtt_router_config.yaml'))
-
 def usage
-  puts "usage : #{$_} sub_topic pub_topic regexp format"
-  puts ""
-  puts "    example"
-  puts %q!        ./mqtt_router office_co2 7seg0003 'co2\":(.+),' 'segd#{sprintf("%04d",md[1].to_i)}'!
-  puts ""
-  puts ""
+  puts <<-EOS
+usage : #{$0} [configuration yaml file]
+
+example :
+   $ vi config.yaml
+       mqtt_host:     mqtt.example.com
+       mqtt_port:     1883
+       mqtt_use_auth: false
+       mqtt_username: username
+       mqtt_password: password
+       mqtt_subscribe_topic: subscribe_topic
+       mqtt_subscribe_regex: co2\":(.+),
+       mqtt_publish_topic: publish_topic
+       mqtt_publish_format: segd\#{sprintf("%04d",md[1].to_i)}
+   $ ./mqtt_router conf.yaml!
+
+EOS
   exit(0)
 end
+usage if ARGV.size != 1
 
-usage if ARGV.size != 4
+$conf = OpenStruct.new(YAML.load_file(ARGV[0]))
 
-$sub_topic = ARGV[0]
-$pub_topic = ARGV[1]
-$regexp    = Regexp.new(ARGV[2])
-$format    = ARGV[3]
+$regexp    = Regexp.new($conf.mqtt_subscribe_regexp)
+$format    = $conf.mqtt_publish_format
 
 def main
   conn_opts = {
-    remote_host: $conf.mqtt_host,
-    remote_port: $conf.mqtt_port,
-    username:    $conf.mqtt_username,
-    password:    $conf.mqtt_password,
+    remote_host: $conf.mqtt_host
   }
+
+  if $conf.mqtt_port > 0
+    conn_opts["remote_port"] = $conf.mqtt_port
+  end
+
+  if $conf.mqtt_use_auth == true
+    conn_opts["username"] = $conf.mqtt_username
+    conn_opts["password"] = $conf.mqtt_password
+  end
 
   $log.info "connecting..."
   MQTT::Client.connect(conn_opts) do |c|
     $log.info "connected"
-    $log.info "subscribe topic=" + $sub_topic
-    c.get($sub_topic) do |t, msg|
+    $log.info "subscribe topic=" + $conf.mqtt_subscribe_topic
+    c.get($conf.mqtt_subscribe_topic) do |t, msg|
       $log.info "received message : msg=" + msg
 
       md = $regexp.match(msg)
@@ -76,7 +95,7 @@ def main
       $log.debug "result=#{result}"
 
       $log.debug "publish : topic=#{$pub_topic}, msg=#{result}"
-      c.publish($pub_topic, result)
+      c.publish($conf.mqtt_publish_topic, result)
     end
   end
 end
